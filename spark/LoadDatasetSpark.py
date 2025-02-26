eb97a356d77b10911001c812f39633572df79bf0
vikas-705 <vikasandotra1998@gmail.com>
2025-02-26 15:04:00 +0530

    Delete spark directory

--- spark/LoadDatasetSpark.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("TimeSeriesPrediction").getOrCreate()
-data = spark.read.csv("/data/input/cleaned_dataset.csv", header=True, inferSchema=True)
-data.show()
--- spark/TrainMachine.py
+++
@@ -1,6 +0,0 @@
-from pyspark.ml.regression import LinearRegression
-
-lr = LinearRegression(featuresCol="features", labelCol="Temperature")
-model = lr.fit(data)
-predictions = model.transform(data)
-predictions.show()
